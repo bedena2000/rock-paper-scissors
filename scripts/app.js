02c41b890f7ceb88380ed390f562898cdb9632e0
bedena2000 <bedena2000@gmail.com>
2022-02-19 16:19:55 +0400

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,127 @@
+// Step 1: 
+let score = 0;
+function userAnswer() {
+  let userAnswer = prompt('Hey choose one of the three: Rock, Paper, Scissors').toLowerCase();
+  while(userAnswer !== 'rock' && userAnswer !== 'paper' && userAnswer !== 'scissors') {
+    userAnswer = prompt('Hey choose one of the three: Rock, Paper, Scissors').toLowerCase();
+  }
+  return userAnswer;
+}
+// Sigil of new Game
+let newGameSigil = false;
+// Step 2:
+const gameArray = ['rock', 'paper', 'scissors'];
+function computerAnswer() {
+  // Step 1: Make Random Number 0, 1, 2
+  const randomNumber = Math.floor(Math.random() * 3);
+  return gameArray[randomNumber];
+}
+
+function computerWinner() {
+  document.querySelector(".draw").textContent = '';
+  // Get computer scrore
+  let computerScore = Number(document.querySelector(".computer").textContent);
+  // increaseScore 
+  computerScore++;
+  // Set the new score
+  document.querySelector(".computer").textContent = computerScore;
+};
+
+function userWinner() {
+  document.querySelector(".draw").textContent = '';
+  // Get computer scrore
+  let userScore = Number(document.querySelector(".user").textContent);
+  // increaseScore 
+  userScore++;
+  // Set the new score
+  document.querySelector(".user").textContent = userScore;
+};
+
+function draw() {
+  document.querySelector(".draw").textContent = 'Draw';
+}
+
+function clearFunction() {
+  document.querySelector(".computer").textContent = '0';
+  document.querySelector(".user").textContent = '0';
+  document.querySelector(".draw").textContent = '';
+}
+
+function checkSigil() {
+  let computerScore = Number(document.querySelector(".computer").textContent);
+  let userScore = Number(document.querySelector(".user").textContent);
+  if(computerScore === 3 || userScore === 3) {
+    newGameSigil = true;
+  }
+}
+
+function startGame() {
+  const userChoose = userAnswer();
+  const computerChoose = computerAnswer();
+  const winner = checkWinner(userChoose, computerChoose);
+  if(winner === 'user') {
+    if(newGameSigil) {
+      clearFunction();
+      newGameSigil = false;
+    }
+    userWinner();
+    checkSigil()
+  } else if (winner === 'computer') {
+    if(newGameSigil) {
+      clearFunction();
+      newGameSigil = false;
+    }
+    computerWinner();
+    checkSigil()
+  } else if (winner === 'draw') {
+    draw();
+  }
+}
+function checkWinner(userChoose, computerChoose) {
+  let winnerState;
+  if(
+    userChoose === 'rock' && computerChoose === 'paper'
+  ) {
+    winnerState = 'computer';
+  } else if (
+    userChoose === 'rock' && computerChoose === 'scissors'
+  ) {
+    winnerState = 'user';
+  } else if (
+    userChoose === 'rock' && computerChoose === 'rock'
+  ) {
+    winnerState = 'draw';
+  } else if (
+    userChoose === 'paper' && computerChoose === 'rock'
+  ) {
+    winnerState = 'user';
+  } else if (
+    userChoose === 'paper' && computerChoose === 'paper'
+  ) {
+    winnerState = 'draw';
+  } else if (
+    userChoose === 'paper' && computerChoose === 'scissors'
+  ) {
+    winnerState = 'computer';
+  } else if (
+    userChoose === 'scissors' && computerChoose === 'rock'
+  ) {
+    winnerState = 'computer';
+  } else if (
+    userChoose === 'scissors' && computerChoose === 'paper'
+  ) {
+    winnerState = 'user';
+  } else if (
+    userChoose === 'scissors' && computerChoose === 'scissors'
+  ) {
+    winnerState = 'draw';
+  }
+  return winnerState;
+}
+// Final Logic
+const button = document.querySelector(".start-game");
+button.addEventListener("click", (event) => {
+  startGame();
+});
+
+
